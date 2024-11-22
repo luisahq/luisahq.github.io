9b87b26a7c466d370b09126f18e33d765d0be69e
luisahq <135194927+luisahq@users.noreply.github.com>
2024-11-22 23:31:07 +1100

    quiz_converter: normalize quiz

--- _plugins/quiz_converter.rb
+++ _plugins/quiz_converter.rb
@@ -1,4 +1,4 @@
-require "perfect_toml"
+require 'perfect_toml'
 
 module Jekyll
   class QuizConverter < Converter
@@ -9,23 +9,108 @@ module Jekyll
       ext =~ /^\.md$/i
     end
 
-    def output_ext(ext)
+    def output_ext (ext)
       '.html'
     end
 
-    def convert_quiz (node)
-      if node.type == :codeblock && node.options[:lang] == 'quiz'
-        hashmap = PerfectTOML.parse node.value
+    def normalize_question_type (question)
+      if !question.has_key? 'type'
+        raise 'Question must have a type.'
       end
 
-      node.children.each do |child|
-        convert_quiz child
+      question['type'] = question['type'].delete(' ').downcase
+
+      if question['type'] != 'shortanswer' &&
+         question['type'] != 'multiplechoice'
+        raise 'Question type must be either short answer or multiple choice.'
       end
+    end
+
+    def is_bool? (object)
+      object.is_a?(TrueClass) || object.is_a?(FalseClass)
+    end
+
+    def normalize_question (question)
+      if !question.instance_of? Hash
+        raise 'Question must be a table.'
+      end
+
+      if !question.has_key? 'question'
+        raise 'Question must have a "question" key.'
+      end
+
+      if question.has_key? 'context'
+        if !question['context'].instance_of? String
+          raise 'Context must be a string.'
+        end
+      end
+
+      normalize_question_type question
+
+      if question['type'] == 'shortanswer'
+        if !question.has_key? 'answer'
+          raise 'Short answer question must have an "answer" key.'
+        end
+
+        if question.has_key?('ci') && !is_bool?(question['ci'])
+          raise 'ci must be a boolean.'
+        end
+
+        if question.has_key?('fuzz') && !question['fuzz'].is_a?(Numeric)
+          raise 'Fuzz factor must be a number.'
+        end
+      end
+
+      if question['type'] == 'multiplechoice'
+        if !question.has_key? 'choices'
+          raise 'Multiple choice question must have a "choices" key.'
+        end
+
+        if !question['choices'].instance_of? Array
+          raise 'Choices must be an array.'
+        end
+
+        if !question.has_key? 'answers'
+          raise 'Multiple choice question must have a "answers" key.'
+        end
+
+        if !question['answers'].instance_of? Array
+          raise 'Answers must be an array.'
+        end
+
+        question['answers'].each do |answer|
+          if !answer.is_a? Integer
+            raise 'Multiple choice answer must be an integer.'
+          end
+        end
+      end
+    end
+
+    def normalize_quiz (quiz)
+      if !quiz.has_key? 'questions'
+        raise 'Quiz does not have any questions.'
+      end
+
+      if !quiz['questions'].instance_of? Array
+        raise 'Questions must be an array.'
+      end
+
+      quiz['questions'].each do |question| normalize_question question end
+    end
+
+    def convert_quizzes (node)
+      if node.type == :codeblock && node.options[:lang] == 'quiz'
+        quiz = PerfectTOML.parse node.value
+        normalize_quiz quiz
+        p quiz
+      end
+
+      node.children.each do |child| convert_quizzes child end
     end
 
     def convert (content)
       doc = Kramdown::Document.new content, input: 'GFM'
-      convert_quiz doc.root
+      convert_quizzes doc.root
       doc.to_html
     end
   end
--- quiz_demo.md
+++ quiz_demo.md
@@ -5,5 +5,19 @@ within: programming
 ---
 
 ```quiz
-q1
+[[questions]]
+type = "short answer"
+question = "What keyword is used to define a function in Python?"
+answer = "def"
+
+[[questions]]
+type = "multiple choice"
+question = "Which of the following is a valid variable name in Python?"
+choices = [
+  "1_variable",
+  "variable-name",
+  "variable_name",
+  "class"
+]
+answers = [3]
 ```
