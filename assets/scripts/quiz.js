2237b2e7f1e54e3e804aa149fdd73128b54098d8
luisahq <135194927+luisahq@users.noreply.github.com>
2024-12-05 07:00:05 +1100

    quiz.js: save answer submissions to localStorage and use this data to render

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -6,72 +6,114 @@ function parentUntil (el, query) {
     return parentUntil(el.parentElement, query)
 }
 
-function compareAnswers (type, submitted, correct) {
-    if (submitted === correct) return 'correct'
+function calcScore (type, submitted, correct) {
+    if (submitted === correct) return 100
 
-    if (type === 'multiplechoice' && correct.indexOf(',') !== -1) {
-        const correctAnswers = correct.split(',')
-        const submittedAnswers = submitted.split(',')
+    if (type === 'ma') {
+        const corrects = correct.split(',')
+        const submitteds = submitted.split(',')
         let n = 0
 
-        submittedAnswers
-            .forEach(a => n += correctAnswers.indexOf(a) === -1 ? 0 : 1)
+        for (const a of submitteds) {
+            if (corrects.indexOf(a) === -1) return 0
+            n += 1
+        }
 
-        return n === correctAnswers.length ? 'correct' : 'partial'
+        return n === corrects.length
+            ? 100
+            : n === 0 ? 0 : Math.round(100 * n / corrects.length)
     }
 
-    return 'incorrect'
-}
-
-function view () {
-    document.querySelectorAll('div.quiz').forEach(el => {
-        const state = localStorage.getItem(el.id)
-        if (state === null) return
-        el.classList.add(state)
-
-        el.querySelectorAll('fieldset').forEach(f => {
-            const inputs = f.querySelectorAll('input')
-            inputs.forEach(input => input.setAttribute('disabled', ''))
-            const submittedAnswer = localStorage.getItem(f.id)
-            if (submittedAnswer === null) return
-            const correctAnswer = f.dataset.answer
-
-            const result = compareAnswers(
-                f.dataset.questionType,
-                submittedAnswer,
-                correctAnswer
-            )
-
-            f.dataset.correct = result
-        })
-    })
+    return 0
 }
 
 function save () {
     const quiz = document.querySelector('div.quiz.active')
+    if (quiz === null) return;
+    let not100 = 0
 
-    quiz
-        .querySelectorAll('fieldset')
-        .forEach(f => {
-            if (f.dataset.questionType === 'shortanswer')
-                localStorage.setItem(f.id, f.querySelector('input').value)
+    for (const f of quiz.querySelectorAll('fieldset')) {
+        // Save student answers for each question in active quiz.
+        if (f.dataset.questionType === 'sa')
+            localStorage.setItem(f.id, f.querySelector('input').value)
 
-            const answers = []
+        else if (f.dataset.questionType === 'mc') {
+            const inputs = f.querySelectorAll('input')
+            let i = 0
+            for (; i < inputs.length; ++i)
+                if (inputs[i].checked) {
+                    localStorage.setItem(f.id, (i + 1).toString())
+                    break
+                }
 
+            if (i === inputs.length) localStorage.setItem(f.id, '')
+        }
+
+        else {
+            const checked = []
             f
                 .querySelectorAll('input')
-                .forEach((el, i) => { if (el.checked) answers.push(i + 1) })
+                .forEach((el, i) => {
+                    if (el.checked) checked.push((i + 1).toString())
+                })
 
-            localStorage.setItem(f.id, answers.map(n => n.toString()).join(','))
-        })
+            localStorage.setItem(f.id, checked.join(','))
+        }
 
-    let notCorrect = 0
+        // Calculate score (percentage mark) for each question, save it, and use
+        // these to determine if quiz should be marked as only attempted, or as
+        // complete if every answer is correct.
+        const score = calcScore(
+            f.dataset.questionType,
+            localStorage.getItem(f.id),
+            f.dataset.answer
+        )
 
-    quiz
-        .querySelectorAll('fieldset')
-        .forEach(f => { if (f.dataset.correct !== 'correct') ++notCorrect })
+        if (score !== 100) ++not100;
+        localStorage.setItem(f.id + 'Score', score.toString())
+    }
 
-    localStorage.setItem(quiz.id, notCorrect > 0 ? 'attempted' : 'complete')
+    localStorage.setItem(quiz.id, not100 > 0 ? 'attempted' : 'complete')
+}
+
+function render () {
+    document.querySelectorAll('div.quiz').forEach(el => {
+        const state = localStorage.getItem(el.id)
+        if (state === null) return
+        if (state === 'complete') el.classList.remove('attempted')
+        el.classList.add(state)
+
+        for (const f of el.querySelectorAll('fieldset')) {
+            const inputs = f.querySelectorAll('input')
+            const submitted = localStorage.getItem(f.id)
+
+            if (f.dataset.questionType === 'sa') inputs[0].value = submitted
+            else if (f.dataset.questionType === 'mc') {
+                if (submitted !== '')
+                    inputs[parseInt(submitted, 10) - 1].checked = true
+            }
+
+            else {
+                if (submitted !== '')
+                    submitted
+                        .split(',')
+                        .map(x => parseInt(x, 10))
+                        .forEach(n => { inputs[n - 1].checked = true })
+            }
+
+            const _score = localStorage.getItem(f.id + 'Score')
+            f.dataset.score = _score
+            if (!el.classList.contains('active'))
+                inputs.forEach(input => input.setAttribute('disabled', ''))
+
+            else {
+                if (_score === null || _score !== '100')
+                    inputs.forEach(input => input.removeAttribute('disabled'))
+
+                else inputs.forEach(input => input.setAttribute('disabled', ''))
+            }
+        }
+    })
 }
 
 function activateQuiz (el) {
